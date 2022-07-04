07cbca01cf720c840055fff53508c6b44aabb4d6
eya-harbaoui <102240094+eya-harbaoui@users.noreply.github.com>
2022-07-04 16:02:19 +0100

    debut frontend
    login page without css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Login } from "./components/login/index";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Login></Login>
+    </div>
+    
+    
+  );
+}
+
+export default App;
---
+++ frontend/src/components/login/login.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export class Login extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+          <div className="base-container">
+            <div className="header">Connexion</div>
+            <div className="content">
+              <div className="image"></div>
+            </div>
+            <div className="form">
+
+              <div className="form-group">
+                <label htmlFor="username">Nom de l'utilisateur</label>
+                <input type="text" placeholder="nom de l'utilisateur" name='username' />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="password">Mot de passe</label>
+                <input type="password" placeholder='mot de passe' name='password' />
+              </div>
+
+            </div>
+
+            <div className='footer'>
+                <button type='button' className='btn'>se connecter</button>
+            </div>
+          </div>
+        );
+    }
+}
