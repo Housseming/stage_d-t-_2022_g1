aa6742d0bfe67f00a6530d2be6b36b7d58da82eb
Aziz <takaliaziz4@gmail.com>
2022-07-06 14:59:57 +0100

    front fini manque design finale

---
+++ frontend/src/components/login/register.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {VscAccount} from "react-icons/vsc";
+
+export class Register extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="base-container">
+        <div className="header">Enregistrez-vous</div>
+        <div className="content">
+          <VscAccount className="image"></VscAccount>
+          </div>
+        <div className="form">
+          <div className="form-group">
+            <label htmlFor="username">Nom de l'utilisateur</label>
+            <input
+              type="text"
+              placeholder="nom de l'utilisateur"
+              name="username"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password">Email</label>
+            <input type="email" placeholder="email" name="password" />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password">Mot de passe</label>
+            <input type="password" placeholder="mot de passe" name="password" />
+          </div>
+        </div>
+        <div className="footer">
+          <button type="button" className="btn">
+            Enregistrez-vous
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
