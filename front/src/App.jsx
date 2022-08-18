d7b6a498f93a20dfa2bf332f611463f11d3400ad
Aziz <takaliaziz4@gmail.com>
2022-08-18 11:52:11 +0100

    updt

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import React from "react";
 //import Axios from 'axios';
-import Parametreglobale from "./components/dashboard/Parametreglobale"
+import Parametreglobale from "./components/dashboard/Parametreglobale";
 import PrimeHuissier from "./components/dashboard/primehuissier.jsx";
-import { Sharedlayout } from "./components/dashboard/sharedLayout.jsx";
-import { AccountBox, RegisterBox } from "./components/accountBox/index.jsx";
-import { ToastContainer } from "react-toastify";
+import {Sharedlayout} from "./components/dashboard/sharedLayout.jsx";
+import {AccountBox, RegisterBox} from "./components/accountBox/index.jsx";
+import {ToastContainer} from "react-toastify";
 import Gestionclient from "./components/dashboard/Gestionclient.jsx";
 import "react-toastify/dist/ReactToastify.css";
 import Honoraireenextra from "./components/dashboard/honoraireenextra.jsx";
@@ -20,7 +20,7 @@ import Tribunale from "./components/dashboard/tribinaux_administrations/tribunal
 import Welcome from "./components/welcome/welcome";
 import RechercheDossier from "./components/dashboard/recherchedossier";
 import Home from "./components/welcome/Home";
-import { Collabo } from "./components/dashboard/collaborateurs/collabo";
+import {Collabo} from "./components/dashboard/collaborateurs/collabo";
 import CreationDossier from "./components/dashboard/creationdossier";
 import Footer from "./components/FOOTER/footer";
 import Dossiers from "./components/dashboard/Dossiers";
@@ -30,33 +30,39 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/login" element={<AccountBox />} />
-        <Route path="/register" element={<RegisterBox />} />
-
-        <Route path="/home" element={<Sharedlayout />}>
-          <Route path="primehuissier" element={<PrimeHuissier />} />
-          <Route path="utilisateur" element={<Utilisateur />} />
-          <Route path="Parametreglobale" element={<Parametreglobale />} />
-          <Route path="honoraireenextra" element={<Honoraireenextra />} />
-          <Route path="/home/timbre" element={<Navigate to="/home/timbre" />} />
-          <Route path="photocopie" element={<Photocopie />} />
-          <Route path="transport" element={<Transport />} />
-          <Route path="recettedufinance" element={<Recettedufinance />} />
-          <Route path="emplacementdossier" element={<Emplacementdossier />} />
-          <Route path="gestionclient" element={<Gestionclient />} />
-          <Route path="collaborateurs" element={<Collabo></Collabo>}></Route>
-          <Route path="Tribunaux_et_administrations" element={<Tribunale />} />
-          <Route path="welcome" element={<Welcome></Welcome>}></Route>
-          <Route path="recherchedossier" element={<RechercheDossier />}></Route>
-          <Route path="creationdossier" element={<CreationDossier />}></Route>
-          <Route path="dossiers" element={<Dossiers />}></Route>
+        <Route path="/">
+          <Route index element={<Home></Home>}></Route>
+          <Route path="login" element={<AccountBox />} />
+          <Route path="register" element={<RegisterBox />} />
+          <Route path="home" element={<Sharedlayout />}>
+            <Route path="primehuissier" element={<PrimeHuissier />} />
+            <Route path="utilisateur" element={<Utilisateur />} />
+            <Route path="Parametreglobale" element={<Parametreglobale />} />
+            <Route path="honoraireenextra" element={<Honoraireenextra />} />
+            <Route path="timbre" element={<Timbre />} />
+            <Route path="photocopie" element={<Photocopie />} />
+            <Route path="transport" element={<Transport />} />
+            <Route path="recettedufinance" element={<Recettedufinance />} />
+            <Route path="emplacementdossier" element={<Emplacementdossier />} />
+            <Route path="gestionclient" element={<Gestionclient />} />
+            <Route path="collaborateurs" element={<Collabo></Collabo>}></Route>
+            <Route
+              path="Tribunaux_et_administrations"
+              element={<Tribunale />}
+            />
+            <Route path="welcome" element={<Welcome></Welcome>}></Route>
+            <Route
+              path="recherchedossier"
+              element={<RechercheDossier />}></Route>
+            <Route path="creationdossier" element={<CreationDossier />}></Route>
+            <Route path="dossiers" element={<Dossiers />}></Route>
+          </Route>
         </Route>
       </Routes>
       {/*<Footer></Footer>*/}
       <ToastContainer position="top-center"></ToastContainer>
     </BrowserRouter>
   );
-                  }
+}
 
 export default App;
