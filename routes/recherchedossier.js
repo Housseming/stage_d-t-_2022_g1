38222ee8f5b6e2d80423acceddf852692eeae44b
eya-harbaoui <102240094+eya-harbaoui@users.noreply.github.com>
2022-09-01 01:35:11 +0100

    update creation et rechercher dossier

--- routes/recherchedossier.js
+++ routes/recherchedossier.js
@@ -19,16 +19,43 @@ const pool = require("../db");
     console.error(err.message);
   }
 });*/
-
-root11.post("/recherchedossieradd", async (req, res) => {
+//ajouter les données du client du dossier concernant  
+root11.post("/clientdossieradd", async (req, res) => {
   try {
-    const { matricule, raison, num, activité, categorie, situation_fiscale } =
+    const {raison,num} =
       req.body;
 
     const newdossiers = await pool.query(
-      "INSERT INTO recherchedossier (client,tel) VALUES($1,$2)",
+      "INSERT INTO recherchedossier (client,tel) VALUES($1,$2) RETURNING id_dossier",
       [raison, num]
-      //res.json("succes"),
+    );
+    console.log(newdossiers.rows[0])
+    res.json(newdossiers.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+//"UPDATE  recherchedossier SET (num_affaire,emplacement,mission,code_dossier,annee,lieu,service,observation,date_creation,type_dossier) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10) WHERE id_dossier=$11",
+//ajouter les donnée du dossier
+root11.post("/donneedossieradd", async (req, res) => {
+  try {
+    const {typedossier,
+    codedossier,
+    annee,
+    mission,
+    emplacement,
+    numaffaire,
+    lieu,
+    service,
+    observation,
+    date_creation,
+    id_dossier} = req.body;
+
+    const newdossiers = await pool.query(
+      
+      "UPDATE recherchedossier SET num_affaire=$1,emplacement=$2,mission=$3,code_dossier=$4,annee=$5,lieu=$6,service=$7,observation=$8,date_creation=$9,type_dossier=$10 WHERE id_dossier=$11",
+      [numaffaire,emplacement,mission,codedossier,annee,lieu,service,observation,date_creation,typedossier,id_dossier]
+     
     );
     res.json(newdossiers.rows[0]);
   } catch (err) {
