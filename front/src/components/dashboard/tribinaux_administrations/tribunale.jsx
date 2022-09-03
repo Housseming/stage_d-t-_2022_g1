5b99ecc4835d235ea8ad7d885403f00dd3cddb43
eya-harbaoui <102240094+eya-harbaoui@users.noreply.github.com>
2022-09-03 01:37:12 +0100

    modifier service

--- front/src/components/dashboard/tribinaux_administrations/tribunale.jsx
+++ front/src/components/dashboard/tribinaux_administrations/tribunale.jsx
@@ -324,7 +324,7 @@ const Tribunale = () => {
   const
    editservice = (record) => {
     setEdditingservice({ ...record });
-    setEdditingservice({...edditingservice,tribunale_id:idtrib})
+    setEdditingservice({...edditingservice,tribunale_id:idtrib,service_id:record.service_id})
     setChecked3({
       check1: record.lundi == "audience" ? true : false,
       check2: record.mardi == "audience" ? true : false,
